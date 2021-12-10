96319ab5126e870e5ab0749355c0eb184016ad09
Zuxaw <cascio.hugo@gmail.com>
2021-12-10 22:34:09 +0100

    implementation deepL translation API

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from './user-service';
 import firebase from 'firebase/compat/app';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import 'firebase/compat/auth'; 
 import { User } from './user.model';
 import { Subscription } from 'rxjs';
@@ -13,8 +14,11 @@ import { Subscription } from 'rxjs';
 export class ProfileComponent implements OnInit, OnDestroy {
   user: User;
   userSubscription: Subscription;
+  private deeplUrl = "https://api-free.deepl.com/v2/translate?auth_key=4040bb49-6220-b183-2f26-27ea76565147:fx"
+  toTranslate = "Hello world"
 
-  constructor(private userService :UserService) { 
+  constructor(private userService :UserService,
+              private httpClient :HttpClient) { 
   }
 
   ngOnInit(){
@@ -31,6 +35,27 @@ export class ProfileComponent implements OnInit, OnDestroy {
   async onClick(){
     await this.userService.updateProfile('deffohn');
     // this.getUser();
+  }
+
+  onTranslate(){
+
+    const body = new HttpParams()
+      .set('auth_key','4040bb49-6220-b183-2f26-27ea76565147:fx')
+      .set('text', this.toTranslate)
+      .set('target_lang','JA');
+
+    this.httpClient
+      .post(this.deeplUrl, null ,{params: body})
+      .subscribe(
+        (response) => {
+          console.log(response)
+          console.log(Object.values(response)[0][0].text);
+          this.toTranslate = Object.values(response)[0][0].text;
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
   }
 
   ngOnDestroy(){
