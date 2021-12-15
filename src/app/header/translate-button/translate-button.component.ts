353642ef084e5abf1c29c53442d0160ad91e7f28
Deffohn <maxime13.p@gmail.com>
2021-12-15 11:19:46 +0100

    adding translation button

---
+++ src/app/header/translate-button/translate-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-translate-button',
+  templateUrl: './translate-button.component.html',
+  styleUrls: ['./translate-button.component.scss']
+})
+export class TranslateButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
